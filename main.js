5cc38282344ca8658d14ffa5f35bd9c3d6271f02
xiaoxiaocat <ycc1088@gmial.com>
2022-11-12 20:56:07 +1300

    Change static express.static(path,join(...))

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(express.static('public'))
-
+// app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use("/",database)
 // app.get('/',function(req,res){
